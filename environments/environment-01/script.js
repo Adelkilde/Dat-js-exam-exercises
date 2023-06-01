74cbb4b4610100c6b1aff8022680bcbac59b8797
Adelkilde <William.adelkilde@gmail.com>
2023-06-01 12:22:56 +0200

    Funny hihi

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,42 @@
 "use strict";
 
+window.addEventListener("load", initApp)
+let users = []
+
+async function initApp() {
+    console.log("JavaScript is running 🚀");
+    await getUsers();
+    displayUsers();
+    countRoles();
+}
+
+async function getUsers() {
+    const response = await fetch("users.json");
+    const json = await response.json();
+    users = json;
+}
+
+function displayUsers() {
+    const userList = document.getElementById("userlist");
+    userList.innerHTML = "";
+
+    users.forEach(user => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${user.name} (${user.role})`;
+        userList.appendChild(listItem);
+    });
+}
+
+function countRoles() {
+    const adminCount = users.filter(user => user.role === "admin").length
+    const userCount = users.filter(user => user.role === "user").length
+    const guestCount = users.filter(user => user.role === "guest").length
+
+    const adminCountElement = document.getElementById("admin-count");
+    const userCountElement = document.getElementById("user-count");
+    const guestCountElement = document.getElementById("guest-count");
+
+    adminCountElement.textContent = adminCount;
+    userCountElement.textContent = userCount;
+    guestCountElement.textContent = guestCount;
+}
--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,7 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+function initApp() {
+    console.log("JavaScript is running 🚀");
+}
