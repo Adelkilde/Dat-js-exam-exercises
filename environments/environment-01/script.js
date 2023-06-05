81c9266b3151ee8b850b0a56312ce82b6862fb5e
Adelkilde <William.adelkilde@gmail.com>
2023-06-05 17:28:23 +0200

    Finished!

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,42 +1,58 @@
 "use strict";
 
-window.addEventListener("load", initApp)
-let users = []
+window.addEventListener("load", initApp);
+let users = [];
 
 async function initApp() {
-    console.log("JavaScript is running 🚀");
-    await getUsers();
-    displayUsers();
-    countRoles();
+  console.log("JavaScript is running 🚀");
+  users = await getUsers();
+  console.log(users);
+  displayUsers();
+  countRoles();
 }
 
 async function getUsers() {
-    const response = await fetch("users.json");
-    const json = await response.json();
-    users = json;
+  const response = await fetch("users.json");
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 function displayUsers() {
-    const userList = document.getElementById("userlist");
-    userList.innerHTML = "";
-
-    users.forEach(user => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${user.name} (${user.role})`;
-        userList.appendChild(listItem);
-    });
+  for (const user of users) {
+    const html = /*html*/ `
+    <li>${user.name} (${user.active}) (${user.role})</li>`;
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+  }
 }
 
 function countRoles() {
-    const adminCount = users.filter(user => user.role === "admin").length
-    const userCount = users.filter(user => user.role === "user").length
-    const guestCount = users.filter(user => user.role === "guest").length
+  let adminCount = 0;
+  let userCount = 0;
+  let guestCount = 0;
 
-    const adminCountElement = document.getElementById("admin-count");
-    const userCountElement = document.getElementById("user-count");
-    const guestCountElement = document.getElementById("guest-count");
+  for (const user of users) {
+    if (user.role === "admin") {
+      adminCount++;
+    } else if (user.role === "user") {
+      userCount++;
+    } else if (user.role === "guest") {
+      guestCount++;
+    }
+  }
 
-    adminCountElement.textContent = adminCount;
-    userCountElement.textContent = userCount;
-    guestCountElement.textContent = guestCount;
+  document.getElementById("admin-count").textContent = adminCount;
+  document.getElementById("user-count").textContent = userCount;
+  document.getElementById("guest-count").textContent = guestCount;
 }
+
+/* Lavet før møde med Race d.05/06 */
+// function countRoles() {
+//   const adminCount = users.filter((user) => user.role === "admin").length;
+//   const userCount = users.filter((user) => user.role === "user").length;
+//   const guestCount = users.filter((user) => user.role === "guest").length;
+
+//   document.getElementById("admin-count").textContent = adminCount;
+//   document.getElementById("user-count").textContent = userCount;
+//   document.getElementById("guest-count").textContent = guestCount;
+// }
