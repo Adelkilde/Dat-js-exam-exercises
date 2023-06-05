f98d956cf92f35ad14a4111d82ac38928ead08ea
Adelkilde <William.adelkilde@gmail.com>
2023-06-05 17:28:58 +0200

    Lavet  med forOf!

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -4,39 +4,42 @@ window.addEventListener("load", initApp);
 let users = [];
 
 async function initApp() {
-    console.log("JavaScript is running! 🚀");
-    await getUsers();
-    showActiveUsers();
+  console.log("Data 🚀");
+  await getUsers();
+  showUsers();
+  createUser("Yørgen", true, "guest");
+  createUser("Urt", true, "admin");
+  createUser("Per", false, "guest");
+  console.log(users);
 }
 
 async function getUsers() {
-    const response = await fetch("users.json");
-    const data = response.json
-    return data;
+  const response = await fetch("users.json");
+  users = await response.json();
+  return users;
 }
 
-function showActiveUsers() {
-    const userList = document.getElementById("userlist");
-    userList.innerHTML = "";
-    const activeUsers = users.filter(user => user.active);
-
-    for (const user of activeUsers) {
-        const listItem = document.createElement("li");
-        listItem.textContent = user.name;
-        userList.appendChild(listItem);
+function showUsers() {
+  // const activeUsers = users.filter(user => (user.active));
+  // console.log(activeUsers);
+  document.querySelector("#userlist").innerHTML = "";
+  for (const user of users) {
+    if (user.active) {
+      const html = /*html*/ `
+        <li>${user.name} (${user.active}) (${user.role})</li>
+        `;
+      document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
     }
+  }
 }
 
 function createUser(name, active, role) {
-    const newUser = {
-        name,
-        active,
-        role
-    }
-users.push(newUser);
-showActiveUsers();
-}
+  const user = {
+    name,
+    active,
+    role,
+  };
 
-createUser("Boy", true, "user");
-createUser("Man", false, "guest");
-createUser("Kratos", true, "admin");
+  users.push(user);
+  showUsers();
+}
