849a9d72f4264aa53553968edd9205ce99075813
Adelkilde <William.adelkilde@gmail.com>
2023-06-08 04:55:23 +0200

    1. og 2. done

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -4,12 +4,10 @@ window.addEventListener("load", initApp);
 let playlist = [];
 
 async function initApp() {
-  console.log("JavaScript is running 🚀");
+  console.log("Jaffa");
   await loadPlaylist();
-  showPlaylist();
-  document.querySelector("#removeButton").addEventListener("click", () => {
-    removeSong();
-  });
+  console.log(playlist);
+  displaySongList();
 }
 
 async function loadPlaylist() {
@@ -18,31 +16,7 @@ async function loadPlaylist() {
   playlist = data;
 }
 
-function addNewSong(event) {
-  event.preventDefault();
-  const form = event.target;
-  const artist = form.name.value;
-  const title = form.title.value;
-  const duration = form.duration.value;
-  newSong(artist, title, duration);
-  showPlaylist();
-  form.reset();
-}
-
-function newSong(artist, title, duration) {
-  const song = { artist, title, duration };
-  playlist.push(song);
-}
-
-function removeSong(song) {
-  const index = playlist.indexOf(song);
-  if (index > -1) {
-    playlist.splice(index, 1);
-  }
-  showPlaylist();
-}
-
-function showPlaylist() {
+function displaySongList() {
   document.querySelector("#songlist").innerHTML = "";
 
   for (const song of playlist) {
@@ -52,25 +26,3 @@ function showPlaylist() {
     document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
   }
 }
-
-// const listItem = document.createElement("li");
-// listItem.textContent = `${song.artist}: ${song.title} (${song.duration})`;
-
-// const removeButton = document.createElement("button");
-// removeButton.textContent = "Remove";
-// removeButton.addEventListener("click", () => {
-//   removeSong(song);
-// });
-
-// listItem.appendChild(removeButton);
-// songList.appendChild(listItem);
-
-// function showPlaylist() {
-//   document.querySelector("#songlist").innerHTML = "";
-
-//   for (const song of playlist) {
-//     const html = /*html*/ `
-//     <li>${song.artist}: ${song.title} (${song.duration})</li>`;
-//     document.querySelector("#songslist").insertAdjacentHTML("beforeend", html);
-//   }
-// }
