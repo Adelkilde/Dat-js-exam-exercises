8251adbc480b9bed8733b80d2895b3e5f44886a8
Adelkilde <William.adelkilde@gmail.com>
2023-06-11 05:23:18 +0200

    21 ✅ 24 ✅

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+const students = [];
+
+function initApp() {
+  console.log("JavaScript is running 🤘");
+  document.querySelector("#create-student-form").addEventListener("submit", addStudent);
+  console.log(students);
+}
+
+function addStudent(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = Number(form.age.value);
+  createStudent(name, email, age);
+  showStudents(students);
+}
+
+function createStudent(name, email, age) {
+  const newStudent = { name, email, age };
+  students.push(newStudent);
+}
+
+function showStudents(listOfStudents) {
+  document.querySelector("#students-table-body").innerHTML = "";
+  listOfStudents.sort((student1, student2) => student1.name.localeCompare(student2.name));
+
+  for (const student of listOfStudents)
+    if (student.age > 18 - 1) {
+      const html = /*html*/ `
+        <tr>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.age}</td>
+        </tr>`;
+      document.querySelector("#students-table-body").insertAdjacentHTML("beforeend", html);
+    } else {
+      console.log("Student under 18 - Added, but not shown");
+    }
+}
--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+const songs = [
+  { artist: "Deftones", title: "Rosemary", duration: "3:21" },
+  { artist: "Deftones", title: "Entombed", duration: "3:21" },
+  { artist: "Nickelback", title: "Far Away", duration: "3:21" },
+  { artist: "Nickelback", title: "Savin' Me", duration: "3:21" },
+  { artist: "Slipknot", title: "Iowa", duration: "3:21" },
+];
+
+function initApp() {
+  console.log("JavaScript is running 🤘");
+  document.querySelector("#add-song-form").addEventListener("submit", addSong);
+  document.querySelector("#sort-songs-form").addEventListener("change", sortByClicked);
+  displaySongs();
+}
+
+function addSong(event) {
+  event.preventDefault();
+  const form = event.target;
+  const artist = form.name.value;
+  const title = form.title.value;
+  const duration = form.duration.value;
+  createSong(artist, title, duration);
+  displaySongs();
+}
+
+function createSong(artist, title, duration) {
+  const newSong = { artist, title, duration };
+  songs.push(newSong);
+}
+
+function displaySongs() {
+  document.querySelector("#songlist").innerHTML = "";
+
+  for (const song of songs) {
+    const html = /*html*/ `
+        <li>${song.artist}: ${song.title} (${song.duration})</li>`;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function sortByClicked(event) {
+  if (event.target.value == "artist") {
+    songs.sort((song1, song2) => song1.artist.localeCompare(song2.artist));
+  } else {
+    songs.sort((song1, song2) => song1.title.localeCompare(song2.title));
+  }
+  displaySongs();
+}
