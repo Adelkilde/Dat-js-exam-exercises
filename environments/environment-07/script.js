b4b0861c89760eea456019c0f0700195869a2bef
Adelkilde <William.adelkilde@gmail.com>
2023-06-11 02:55:20 +0200

    20 ✅, 23 ✅

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+const students = [];
+
+function initApp() {
+  console.log("JavaScript is running 🤘");
+  document.querySelector("#create-student-form").addEventListener("submit", addStudent);
+}
+
+function addStudent(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = Number(form.age.value);
+  createStudent(name, email, age);
+  showStudents();
+}
+
+function createStudent(name, email, age) {
+  const newStudent = { name, email, age };
+  students.push(newStudent);
+}
+
+function showStudents(listOfStudents) {
+  document.querySelector("#students-table-body").innerHTML = "";
+  listOfStudents = students.sort((student1, student2) => student1.age - student2.age);
+
+  for (const student of listOfStudents) {
+    const html = /*html*/ `
+    <tr>
+        <td>${student.name}</td>
+        <td>${student.email}</td>
+        <td>${student.age}</td>
+    </tr>`;
+    document.querySelector("#students-table-body").insertAdjacentHTML("beforeend", html);
+  }
+}
--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+const songs = [
+  { artist: "Nickelback", title: "Rockstar", duration: "3:21", vote: 0 },
+  { artist: "Nickelback", title: "Far Away", duration: "3:21", vote: 0 },
+  { artist: "Nickelback", title: "Savin' Me", duration: "3:21", vote: 0 },
+];
+
+function initApp() {
+  console.log("JavaScript is running 🚀");
+  console.log(songs);
+  displaySongs();
+  //   addSong("Nickelback", "Rockstar", "3:21");
+}
+
+// function addSong(artist, title, duration) {
+//   const newSong = { artist, title, duration };
+//   songs.push(newSong);
+// }
+
+function displaySongs() {
+  document.querySelector("#songlist").innerHTML = "";
+  songs.sort((song1, song2) => song2.vote - song1.vote);
+
+  for (const song of songs) {
+    const html = /*html*/ ` <li>${song.artist}: ${song.title} (${song.duration})<button>👍</button></li>`;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#songlist li:last-child button").addEventListener("click", () => upVoteButton(song));
+  }
+}
+
+function upVoteButton(song) {
+  console.log(song);
+  song.vote++;
+  displaySongs();
+  console.log(songs);
+}
