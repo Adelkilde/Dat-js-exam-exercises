66d47245e0593778bc225f773b4015ab264265e2
Adelkilde <William.adelkilde@gmail.com>
2023-06-12 08:40:56 +0200

    Done og done

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,44 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+const students = [];
+
+function initApp() {
+  console.log("JS is runnin 🤘");
+  document.querySelector("#create-student-form").addEventListener("submit", addStudentToList);
+  console.log(students);
+}
+
+function addStudentToList(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = Number(form.age.value);
+  if (!checkEmail(email)) {
+    console.log("Ugyldig email");
+  } else {
+    createStudent(name, email, age);
+    console.log(students);
+  }
+}
+
+function createStudent(name, email, age) {
+  const newStudent = { name, email, age };
+  students.push(newStudent);
+}
+
+function checkEmail(email) {
+  const requiredEmail = "@stud.kea.dk";
+  const atIndex = email.indexOf("@");
+
+  if (atIndex >= 4 && email.substring(atIndex) === requiredEmail) {
+    return true;
+  } else {
+    const index = students.findIndex((student) => student.email === email);
+    if (index !== -1) {
+      students.splice(index, 1);
+    }
+    return false;
+  }
+}
--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+let songs = [];
+
+async function initApp() {
+  console.log("JS is running 🤘");
+  await getPlaylist();
+  console.log(songs);
+  showPlaylist();
+}
+
+async function getPlaylist() {
+  const response = await fetch("playlist.json");
+  const data = await response.json();
+  songs = data;
+}
+
+function showPlaylist() {
+  document.querySelector("#songlist").innerHTML = "";
+
+  for (const song of songs) {
+    const html = /*html*/ `
+    <li><b>${song.artist}</b>: <b>${song.title}</b> (${song.duration}) <button>Remove</button></li>`;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#songlist li:last-child button").addEventListener("click", () => removeSong(song));
+  }
+}
+
+function removeSong(remove) {
+  console.log(remove);
+  songs = songs.filter((song) => remove !== song);
+  showPlaylist();
+}
